bda4fc7f6e86cfbbddc1aeed694976f33a7a27da
Niclas Axelsson <burbas@183-110-117-82.cust.blixtvik.se>
2009-12-09 23:46:45 +0100

    Remade the architecture. Hopefully this will be for the best.

---
+++ src/irc.erl
@@ -0,0 +1,78 @@
+-module(irc).
+-export([
+    start/5,
+    parse/1,
+    strip_crlf/1,
+    send/2,
+    server_loop/2
+    ]).
+
+
+start(Dispatcher, Server, Port, Username, Realname) ->
+  {ok, Socket} = gen_tcp:connect(Server, Port, [binary, {packet, 0}, {active, false}]),
+  send(Socket, "NICK " ++ Username),
+  send(Socket, "USER " ++ Username ++ " " ++ Realname),
+  server_loop(Dispatcher,Socket).
+
+%%%-----------------------------------------------------------
+%%% @doc
+%%% Parses a string and returns the result
+%%%
+%%% @spec parse(Data::binary()) -> {ok, {MessageType, Data}}
+%%% @end
+%%%-----------------------------------------------------------
+parse(<<"PING :", Data/binary>>) ->
+    {ok, {ping, strip_crlf(binary_to_list(Data))}};
+parse(Data) ->
+    Str = strip_crlf(binary_to_list(Data)),
+    {ok, {general, Str}}.
+ 
+%%%-----------------------------------------------------------
+%%% @doc
+%%% Strips the string from ending '\n\r'
+%%%
+%%% @spec strip_crlf(Str::string()) -> StrippedString
+%%% @end
+%%%-----------------------------------------------------------
+strip_crlf(Str) ->
+    string:strip(string:strip(Str, right, $\n), right, $\r).
+ 
+%%%-----------------------------------------------------------
+%%% @doc
+%%% Sends a message to a socket
+%%%
+%%% @spec send(Socket, Message::string()) -> ok | {error, Reason}
+%%% @end
+%%%-----------------------------------------------------------
+send(Socket, Message) ->
+  gen_tcp:send(Socket, list_to_binary(Text ++ "\r\n")). 
+
+%%%-----------------------------------------------------------
+%%% @doc
+%%% Main loop. Waits for messages and forwards some of them to
+%%% the dispatcher.
+%%%
+%%% @spec server_loop(Dispatcher::pid(), Socket) -> ok
+%%% @end
+%%%-----------------------------------------------------------
+server_loop(Dispatcher, Socket) ->
+  receive 
+    {get_update} ->
+      case gen_tcp:recv(Socket, 0) of 
+        {ok, Packet} ->
+          case parse(Packet) of
+            {ok, {message, Message}} ->
+              Dispatcher ! {message, Message},
+              server_loop(Dispatcher, Socket);
+            {ok, {ping, Data}} ->
+              Dispatcher ! {status, {"Got ping"}},
+              server_loop(Dispatcher, Socket);
+            {ok, {general, Data}} ->
+              Dispatcher ! {status, {Data}},
+              server_loop(Dispatcher, Socket)
+          end;
+        {error, Reason} ->
+          Dispatcher ! {error, Reason},
+          server_loop(Dispatcher, Socket)
+      end
+  end.
